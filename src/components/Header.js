1c63fdd8e9d0984c488aab5b011850125dc97d47
Paul Okello <paulokello273@gmail.com>
2021-05-23 12:40:37 +0300

    💥 NEW: Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+function Header() {
+  return (
+    <header>
+      {/* Top nav */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+        {/* Search */}
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            type="text"
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+        {/* Right */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link hover:underline">
+            <p>Hello Paul</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
+          <div className="link hover:underline">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div className="relative link hover:underline flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full">
+              0
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Bottom nav */}
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <MenuIcon className="h-6 mr-0" />
+        <p className="link hover:underline flex items-center">All</p>
+        <p className="link hover:underline">Prime Video</p>
+        <p className="link hover:underline">Amazon Business</p>
+        <p className="link hover:underline">Today's Deals</p>
+        <p className="link hover:underline hidden lg:inline-flex">
+          Electronics
+        </p>
+        <p className="link hover:underline hidden lg:inline-flex">
+          Food & Delivery
+        </p>
+        <p className="link hover:underline hidden lg:inline-flex">Prime</p>
+        <p className="link hover:underline hidden lg:inline-flex">Buy Again</p>
+        <p className="link hover:underline hidden lg:inline-flex">
+          Shopper Toolkit
+        </p>
+        <p className="link hover:underline hidden lg:inline-flex">
+          Health & Personal Care
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
