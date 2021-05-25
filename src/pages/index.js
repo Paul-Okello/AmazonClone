280af659cdab9cd2e29efd3662369820ae341455
Paul Okello <paulokello273@gmail.com>
2021-05-25 13:54:21 +0300

    📦 Configuration:  Config files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,39 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ products }) {
+  console.log(products);
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon Clone</title>
       </Head>
       {/* Header */}
+      <Header />
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+        {/* Product Feed */}
+        <ProductFeed products={products} />
+      </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  if (!products) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      products,
+    },
+  };
 }
