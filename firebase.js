6ce334c3e50ed5ac34a80b8441c4ada58fcb699a
Paul Okello <paulokello273@gmail.com>
2021-05-26 14:55:42 +0300

    ✨ New Feature: Firebase setup

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBQ2m5Ykf0KifcEw3wah6f47qvfdgTAluc",
+    authDomain: "clone-129bd.firebaseapp.com",
+    projectId: "clone-129bd",
+    storageBucket: "clone-129bd.appspot.com",
+    messagingSenderId: "620643942405",
+    appId: "1:620643942405:web:8df573400a322204839c6c",
+    measurementId: "G-B7JN9NJFSS",
+};
