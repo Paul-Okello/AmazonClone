7a5eefa23cfd92ed4805904b935033c2cef4c486
Paul Okello <paulokello273@gmail.com>
2021-05-27 13:28:50 +0300

    💥 NEW: Pull Data from Redux Store

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,13 @@ import {
 } from "@heroicons/react/outline";
 import { useSession, signIn, signOut } from "next-auth/client";
 import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
 function Header() {
   const [session] = useSession();
   const router = useRouter();
+  const items = useSelector(selectItems);
 
   return (
     <header>
@@ -51,7 +54,7 @@ function Header() {
             onClick={() => router.push("/checkout")}
           >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full">
-              0
+              {items.length}
             </span>
             <ShoppingCartIcon className="h-10" />
             <p className="hidden md:inline font-extrabold md:text-sm mt-2">
