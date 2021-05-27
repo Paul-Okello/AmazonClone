43cf3e93dce8ddd42e84b939aacfc7bb6c6dd1cb
Paul Okello <paulokello273@gmail.com>
2021-05-27 13:28:25 +0300

    💥 NEW: Pull Data From Redux

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,19 +1,34 @@
 import Header from "../components/Header";
 import Image from "next/image";
+import { selectItems } from "../slices/basketSlice";
+import { useSelector } from "react-redux";
+import CheckoutProduct from "../components/CheckoutProduct";
 
 const checkout = () => {
+  const items = useSelector(selectItems);
   return (
     <div className="bg-gray-100">
       <Header />
       <main className="lg:flex max-w-screen-2xl mx-auto">
         {/* Left */}
-        <div>
+        <div className="flex-grow m-5 shadow-sm">
           <Image
             src="https://links.papareact.com/ikj"
             width={1020}
             height={250}
             objectFit="contain"
           />
+          <div className="flex flex-col p-5 space-y-3 bg-white">
+            <h1 className="text-3xl border-b pb-4">
+              {items.length === 0
+                ? "Your Shopping Basket is empty"
+                : "Shopping Basket"}
+            </h1>
+
+            {items.map((item, i) => (
+              <CheckoutProduct />
+            ))}
+          </div>
         </div>
         {/* Right */}
         <div></div>
