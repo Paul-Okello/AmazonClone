5e3c42883e6f0b4a32109f8405fc310efecd7d78
Paul Okello <paulokello273@gmail.com>
2021-05-28 15:41:55 +0300

    ✨ New Feature: Checkout Complete.

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,11 +1,15 @@
 import Header from "../components/Header";
 import Image from "next/image";
-import { selectItems } from "../slices/basketSlice";
+import { selectItems, selectTotal } from "../slices/basketSlice";
 import { useSelector } from "react-redux";
 import CheckoutProduct from "../components/CheckoutProduct";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/client";
 
 const checkout = () => {
+  const total = useSelector(selectTotal);
   const items = useSelector(selectItems);
+  const [session] = useSession();
   return (
     <div className="bg-gray-100">
       <Header />
@@ -41,7 +45,27 @@ const checkout = () => {
           </div>
         </div>
         {/* Right */}
-        <div></div>
+        <div className="flex flex-col bg-white p-10 shadow-md">
+          {items.length > 0 && (
+            <>
+              <h2 className="whitespace-nowrap">
+                Subtotal ({items.length}) items:{" "}
+                <span className="font-bold">
+                  <Currency quantity={total} currency="USD" />
+                </span>
+              </h2>
+              <button
+                disabled={!session}
+                className={`button mt-2 ${
+                  !session &&
+                  "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
+                }`}
+              >
+                {!session ? "Sign in to checkout" : "Proceed to checkout"}
+              </button>
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
