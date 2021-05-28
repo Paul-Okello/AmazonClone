49ca1465425d0d2dc80c4a7ff722a2b949f1358d
Paul Okello <paulokello273@gmail.com>
2021-05-28 16:21:30 +0300

    🐛 FIX: authentication

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,12 +2,12 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
 export default NextAuth({
-    providers: [
-        // OAuth authentication providers...
+  providers: [
+    // OAuth authentication providers...
 
-        Providers.Google({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
-        }),
-    ],
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
 });
