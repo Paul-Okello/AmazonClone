e98c3e27943756dbe84d1bf27919af0b2a6551cc
Paul Okello <paulokello273@gmail.com>
2021-05-28 14:37:53 +0300

    ⚡ General Update:  remove from basket functionality

--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -1,18 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    items: [],
+  items: [],
 };
 
 export const basketSlice = createSlice({
-    name: "basket",
-    initialState,
-    reducers: {
-        addToBasket: (state, action) => {
-            state.items = [...state.items, action.payload];
-        },
-        removeFromBasket: (state, action) => {},
+  name: "basket",
+  initialState,
+  reducers: {
+    addToBasket: (state, action) => {
+      state.items = [...state.items, action.payload];
     },
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex(
+        (basketItem) => basketItem.id === action.payload.id
+      );
+
+      let newBasket = [...state.items];
+
+      if (index >= 0) {
+        //The item exists
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.payload.id}) as it's not in the basket`
+        );
+      }
+      state.items = newBasket;
+    },
+  },
 });
 
 export const { addToBasket, removeFromBasket } = basketSlice.actions;
