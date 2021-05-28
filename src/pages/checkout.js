b8ea1145d1cf7eac09b33e13b6430863b07d8f37
Paul Okello <paulokello273@gmail.com>
2021-05-28 14:37:23 +0300

    🐛 FIX: Checkout right

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -26,7 +26,17 @@ const checkout = () => {
             </h1>
 
             {items.map((item, i) => (
-              <CheckoutProduct />
+              <CheckoutProduct
+                key={i}
+                id={item.id}
+                title={item.title}
+                rating={item.rating}
+                price={item.price}
+                description={item.description}
+                category={item.category}
+                image={item.image}
+                hasPrime={item.hasPrime}
+              />
             ))}
           </div>
         </div>
